c88ded402f2c8a6bf67a9dad7bc778a04ce57ff4
Yura Kozyrenko <yura.mogilev@mail.ru>
2024-06-06 03:39:36 +0300

    fix bot service

--- src/bot/bot.service.ts
+++ src/bot/bot.service.ts
@@ -1,127 +1,41 @@
-import { Injectable } from '@nestjs/common';
-import * as cron from 'node-cron';
+import { Injectable, OnModuleDestroy, OnModuleInit } from '@nestjs/common';
 
-import { Actions, Messages } from './bot.constants';
+import { Actions } from './bot.constants';
+import { BotHandlers } from './bot.handlers';
 import { BotProvider } from './bot.provider';
+import { CronManager } from './cron.manager';
 import { UsersService } from '../users/users.service';
-import getMeteoData from '../utils/getMeteo';
-import sendCitySelection from '../utils/sendCitySelection';
-import sendTimeSelection from '../utils/sendTimeSelection';
-import convertTimeToCron from '../utils/timeToCronValue';
 
 @Injectable()
-export class BotService {
+export class BotService implements OnModuleInit, OnModuleDestroy {
+  private handlers: BotHandlers;
+  private cronManager: CronManager;
   private messageHandlers: Record<string, (chatId: number, message?: string) => Promise<void>>;
-  private userTasks: Map<number, cron.ScheduledTask>;
 
   constructor(
     private readonly bot: BotProvider,
     private readonly usersService: UsersService,
   ) {
+    this.cronManager = new CronManager(bot);
+    this.handlers = new BotHandlers(bot, usersService, this.cronManager);
+
     this.messageHandlers = {
-      [Actions.START]: this.handleStart.bind(this),
-      [Actions.SELECT_CITY]: this.handleSelectCity.bind(this),
-      [Actions.SELECT_TIME]: this.handleSelectTime.bind(this),
-      [Actions.INFO]: this.handleInfo.bind(this),
-      [Actions.EDIT]: this.handleEdit.bind(this),
-      default: this.handleDefault.bind(this),
+      [Actions.START]: this.handlers.handleStart.bind(this.handlers),
+      [Actions.SELECT_CITY]: this.handlers.handleSelectCity.bind(this.handlers),
+      [Actions.SELECT_TIME]: this.handlers.handleSelectTime.bind(this.handlers),
+      [Actions.INFO]: this.handlers.handleInfo.bind(this.handlers),
+      [Actions.EDIT]: this.handlers.handleEdit.bind(this.handlers),
+      default: this.handlers.handleDefault.bind(this.handlers),
     };
-    this.userTasks = new Map();
-  }
-
-  private async handleStart(chatId: number): Promise<void> {
-    const user = await this.usersService.getUserByChatId(chatId);
-
-    if (user) {
-      await this.bot.sendMessage(
-        chatId,
-        `Ваши данные уже сохранены. Хотите изменить город или время? Используйте команду /edit.`,
-      );
-      return;
-    }
-
-    await this.bot.sendMessage(chatId, Messages.START);
-
-    const inlineKeyboard = sendCitySelection();
-
-    await this.bot.sendMessage(chatId, Messages.CITY_SELECTION, {
-      reply_markup: inlineKeyboard,
-    });
-  }
-
-  private async handleSelectCity(chatId: number, city: string): Promise<void> {
-    const user = await this.usersService.getUserByChatId(chatId);
-    if (!user) {
-      await this.usersService.createUser({ chatId, city });
-    } else {
-      await this.usersService.updateUserCity(chatId, { city });
-    }
-
-    await this.bot.sendMessage(chatId, `${Messages.CITY_CONFIRMED} ${city}`);
-    const inlineKeyboard = sendTimeSelection();
-
-    await this.bot.sendMessage(chatId, Messages.TIME_SELECTION, {
-      reply_markup: inlineKeyboard,
-    });
-  }
-
-  private async handleSelectTime(chatId: number, time: string): Promise<void> {
-    await this.usersService.updateUserTime(chatId, { time });
-    const user = await this.usersService.getUserByChatId(chatId);
-    this.scheduleUserTask(chatId, time, user.city);
-    await this.bot.sendMessage(chatId, `${Messages.CITY_CONFIRMED} ${user.city} ${Messages.TIME_CONFIRMED} ${time}`);
-  }
-
-  private async handleInfo(chatId: number): Promise<void> {
-    const user = await this.usersService.getUserByChatId(chatId);
-
-    if (!user) {
-      await this.bot.sendMessage(chatId, `Заполните сначала поля выбора города и время`);
-      return;
-    }
-
-    const { city, time } = user;
-    await this.bot.sendMessage(chatId, `${Messages.CITY_CONFIRMED} ${city} ${Messages.TIME_CONFIRMED} ${time}`);
-  }
-
-  private async handleEdit(chatId: number): Promise<void> {
-    await this.bot.sendMessage(chatId, Messages.EDIT_CITY, {
-      reply_markup: sendCitySelection(),
-    });
-  }
-
-  private async handleDefault(chatId: number, city: string): Promise<void> {
-    const result = await getMeteoData(city);
-    await this.bot.sendMessage(chatId, result);
   }
 
   async onModuleInit() {
-    await this.scheduleUserTasks();
+    const users = await this.usersService.getAllUsers();
+    users.forEach((user) => this.cronManager.scheduleUserTask(user.chatId, user.time, user.city));
   }
 
   async onModuleDestroy() {
-    this.userTasks.forEach((task) => task.stop());
-  }
-
-  private async scheduleUserTasks() {
-    const users = await this.usersService.getAllUsers();
-    for (const user of users) {
-      this.scheduleUserTask(user.chatId, user.time, user.city);
-    }
-  }
-
-  private async scheduleUserTask(chatId: number, time: string, city: string) {
-    if (this.userTasks.has(chatId)) {
-      this.userTasks.get(chatId).stop();
-    }
-
-    const cronTime = convertTimeToCron(time);
-    const task = cron.schedule(cronTime, async () => {
-      const meteoData = await getMeteoData(city);
-      await this.bot.sendMessage(chatId, meteoData);
-    });
-
-    this.userTasks.set(chatId, task);
+    this.cronManager.stopAllTasks();
   }
 
   async sendMessage(chatId: number, message: string) {
@@ -130,17 +44,16 @@ export class BotService {
   }
 
   async handleCallbackQuery(callbackQuery: any) {
-    const [action] = callbackQuery.data.split(':');
+    const [action, ...data] = callbackQuery.data.split(':');
     const chatId = callbackQuery.message.chat.id;
 
     if (action === Actions.SELECT_CITY) {
-      const [, city] = callbackQuery.data.split(':');
-      await this.messageHandlers[Actions.SELECT_CITY](chatId, city);
+      await this.handlers.handleSelectCity(chatId, data[0]);
     }
 
     if (action === Actions.SELECT_TIME) {
-      const [, hours, minutes] = callbackQuery.data.split(':');
-      await this.messageHandlers[Actions.SELECT_TIME](chatId, `${hours}:${minutes}`);
+      const [hours, minutes] = data;
+      await this.handlers.handleSelectTime(chatId, `${hours}:${minutes}`);
     }
   }
 }
